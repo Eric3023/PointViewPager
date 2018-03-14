ff24d93f1ded7ce051a683467322ebaabc46bcf2
Eric3023 <2018215108@qq.com>
2018-03-14 16:01:25 +0800

    :)

--- pointviewpager/src/main/java/com/dong/pointviewpager/adapter/PointPagerAdapter.java
+++ pointviewpager/src/main/java/com/dong/pointviewpager/adapter/PointPagerAdapter.java
@@ -1,6 +1,5 @@
 package com.dong.pointviewpager.adapter;
 
-import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,39 +13,26 @@ import java.util.List;
 
 public class PointPagerAdapter extends PagerAdapter {
 
-    private Context mContext;
-    private List<String> mImageStrings;
-    private List<Integer> mImageResources;
+    private List<ImageView> mList;
     private boolean isLoop;
     private boolean isAuto;
     private int mAutoTime;
-    private ImageView imageView;
 
-    public PointPagerAdapter(Context context, List<String> imageStrings, List<Integer> imageResources, boolean isLoop, boolean isAuto, int mAutoTime) {
-        this.mContext = context;
-        this.mImageStrings = imageStrings;
-        this.mImageResources = imageResources;
+    public PointPagerAdapter(List<ImageView> list, boolean isLoop, boolean isAuto, int mAutoTime) {
+        this.mList = list;
         this.isLoop = isLoop;
         this.isAuto = isAuto;
         this.mAutoTime = mAutoTime;
-
-        imageView = new ImageView(mContext);
-        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
     }
 
     @Override
     public int getCount() {
-        if(mImageStrings!=null){
+        if(mList!=null)
             if(isLoop)
                 return Integer.MAX_VALUE;
             else
-                return mImageStrings.size();
-        }else{
-            if(isLoop)
-                return Integer.MAX_VALUE;
-            else
-                return mImageResources.size();
-        }
+                return mList.size();
+        return 0;
     }
 
     @Override
@@ -56,14 +42,8 @@ public class PointPagerAdapter extends PagerAdapter {
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
-        if(mImageStrings!=null){
-//            imageView.setImageResource(mImageResources.get(position));
-        }else{
-            if(isLoop)
-            imageView.setImageResource(mImageResources.get(position));
-        }
-        container.addView(imageView);
-        return imageView;
+        container.addView(mList.get(position%mList.size()));
+        return mList.get(position%mList.size());
     }
 
     @Override
