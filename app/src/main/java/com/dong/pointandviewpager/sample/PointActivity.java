a38684b7676b0bf4446a5a5ac9e42478f60ba43e
Eric3023 <2018215108@qq.com>
2018-03-19 16:19:58 +0800

    1.添加点击监听2.修复OUTOFMEMORY3.优化数据源类型4.更改为链式调用

--- app/src/main/java/com/dong/pointandviewpager/sample/PointActivity.java
+++ app/src/main/java/com/dong/pointandviewpager/sample/PointActivity.java
@@ -26,7 +26,7 @@ public class PointActivity extends AppCompatActivity {
     private OnLoopPageChangeListener onLoopPageChangeListener = new OnLoopPageChangeListener() {
         @Override
         protected void onViewPageSelected(int position) {
-            Toast.makeText(PointActivity.this, "选中Position:"+position, Toast.LENGTH_SHORT).show();
+            Log.i("Dong", "选中Position:"+position);
         }
 
         @Override
@@ -43,7 +43,7 @@ public class PointActivity extends AppCompatActivity {
     private OnLoopPagerClickListener onLoopPagerClickListener = new OnLoopPagerClickListener() {
         @Override
         public void onLoopPagerClick(int position, LoopViewPagerBean bean) {
-            Toast.makeText(PointActivity.this, "点击Position:"+position, Toast.LENGTH_SHORT).show();
+            Log.i("Dong", "点击Position:"+position);
         }
     };
 
